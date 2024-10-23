2c60dd012cca219d81fae52324c08c47c78266c9
Surya_Vetsa <110655337+Surya-Bh-ai@users.noreply.github.com>
2024-10-23 20:06:34 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,10 +2,11 @@
 set -e
 
 # Stop the running service on port 5000
-containerID=$(docker ps -q --filter "expose=5000")
+# containerID=$(docker ps -q --filter "expose=5000")
 
-if [ -n "$containerID" ]; then
-    docker rm -f $containerID
-else
-    echo "No container running on port 5000."
-fi
+# if [ -n "$containerID" ]; then
+#     docker rm -f $containerID
+# else
+#     echo "No container running on port 5000."
+# fi
+echo "No container running on port 5000."
